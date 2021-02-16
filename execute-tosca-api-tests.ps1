cd1a3dab231c8f760658ad3231cfe52670175939
jarrod-grayson <52757778+jarrod-grayson@users.noreply.github.com>
2021-02-16 14:44:09 -0500

    Create execute-tosca-api-tests.ps1

---
+++ execute-tosca-api-tests.ps1
@@ -0,0 +1,10 @@
+$ToscaCommanderHome = [System.Environment]::GetEnvironmentVariable('COMMANDER_HOME')
+
+$ToscaCIClientExe = $ToscaCommanderHome + '\ToscaCI\Client\ToscaCIClient.exe'
+
+$m = 'distributed'
+$t = 'junit'
+$r = '"C:\temp\result.xml"'
+$c = '"C:\Program Files (x86)\TRICENTIS\Tosca Testsuite\ToscaCommander\ToscaCI\Client\Config1.xml"'
+
+& $ToscaCIClientExe -m $m -c $c -t $t -r $r
