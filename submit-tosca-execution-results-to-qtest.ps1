d83c7ac0ee29bb6bc7df60b209e2d9d8bc1ab145
jarrod-grayson <52757778+jarrod-grayson@users.noreply.github.com>
2021-07-24 17:44:48 -0400

    Update submit-tosca-execution-results-to-qtest.ps1

--- submit-tosca-execution-results-to-qtest.ps1
+++ submit-tosca-execution-results-to-qtest.ps1
@@ -1,6 +1,6 @@
 $url = 'https://pulse-7.qtestnet.com/webhook/ea99f7b9-7a0a-4dcd-b99d-1a11e7c4c8e3'
 
-$content = Get-Content "C:\Tosca_Projects\ToscaCIResults\result.xml" -Raw
+$content = Get-Content "C:\Tosca_Projects\ToscaCIResults\Results.xml" -Raw
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($content)
 $payload = [System.Convert]::ToBase64String($bytes)
 
