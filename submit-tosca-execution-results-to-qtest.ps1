9153313099876b36d9854e70a04d4ca943be4097
Tricentis-SA <88728221+Tricentis-SA@users.noreply.github.com>
2021-08-18 10:01:56 -0400

    Update submit-tosca-execution-results-to-qtest.ps1

--- submit-tosca-execution-results-to-qtest.ps1
+++ submit-tosca-execution-results-to-qtest.ps1
@@ -1,12 +1,12 @@
-$url = 'https://pulse-7.qtestnet.com/webhook/ea99f7b9-7a0a-4dcd-b99d-1a11e7c4c8e3'
+$url = '<Pulse_Webhook_URL>'
 
-$content = Get-Content "C:\Tosca_Projects\ToscaCIResults\Results.xml" -Raw
+$content = Get-Content "<C:\\path\\to\\results\\filename.ext>" -Raw
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($content)
 $payload = [System.Convert]::ToBase64String($bytes)
 
 $body = @{
- 'projectId' = '87143'
- 'testcycle' = '2292302'
+ 'projectId' = '<ProjectID>'
+ 'testcycle' = '<TestCycleID>'
  'result' = $payload
 }
 
